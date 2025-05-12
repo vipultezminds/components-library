5948b1926ffd043712b3477cc41b8fe41d795459
Vipul Kumar <vipul@tezminds.com>
2025-05-12 18:42:24 +0530

    deleting to reset and retry

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ label, onClick }) => (
-  <button onClick={onClick} style={{ padding: '10px 20px', background: '#007bff', color: '#fff' }}>
-    {label}
-  </button>
-);
-
-export default Button;
